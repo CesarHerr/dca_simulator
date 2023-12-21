9b3a66b1fad9b370e9672d84a1828b7f8167c284
Cesar Herrera <cesarherr321@gmail.com>
2023-12-21 19:58:14 -0300

    Add a footer to the simulator

--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import DataTable from './DataTable';
 import FormData from './FormData';
+import { FaGithub, FaLinkedinIn, FaAddressBook } from "react-icons/fa";
 import useFetchTradesForDateRange from './custom/useFetchTradesForDateRange';
 
 import LineChart from './LineChart';
@@ -31,6 +32,18 @@ function Simulator() {
         </div>
         {isLoading ? <Loader /> : <DataTable />}
       </div>
+      <footer className='md:fixed bottom-0 m-3'>
+      <div className='flex justify-center gap-5 w-full text-xl mb-2'>
+          <a href='https://github.com/CesarHerr' className='hover:scale-125 hover:cursor-pointer transition-all'><FaGithub /></a>
+          <a href='https://www.linkedin.com/in/cesarherr/' className='hover:scale-125 hover:cursor-pointer transition-all'><FaLinkedinIn /></a>
+          <a href='https://cesarherr.github.io/portfolio/' className='hover:scale-125 hover:cursor-pointer transition-all'><FaAddressBook /></a>         
+          </div>
+        <div className='flex flex-col items-center justify-center gap-2 md:flex-row md:justify-between md:items-center md:gap-0'>
+          <p className='text-center text-gray-500 text-xs'>
+            &copy; 2023 DCA Simulator César Herrera González
+          </p>         
+        </div>      
+      </footer>
     </>
   );
 }
