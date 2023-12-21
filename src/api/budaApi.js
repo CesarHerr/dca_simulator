4e0649e5a5f454a5fcd5fc992710d06253ad3a75
Cesar Herrera <cesarherr321@gmail.com>
2023-12-21 19:38:13 -0300

    Config buda api

---
+++ src/api/budaApi.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const marketId = 'btc-clp'
+
+const url = `https://www.buda.com/api/v2/markets/${marketId}/trades`
+
+const fetchTrades = async (timestamp) => {
+  try {
+    const response = await axios.get(url, {
+      params: {
+        timestamp,
+        limit: 1
+      }
+    })
+    const data = response.data
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    throw error
+  }
+}
+
+export default fetchTrades
