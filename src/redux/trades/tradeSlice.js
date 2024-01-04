4e64cad677c331caa4042ec21377a61c834a75ee
Cesar Herrera <cesarherr321@gmail.com>
2024-01-04 18:01:31 -0300

    Add inversion2 and market states

--- src/redux/trades/tradeSlice.js
+++ src/redux/trades/tradeSlice.js
@@ -1,14 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit'
-import fetchTrades from './budaApi'
+import { createSlice } from '@reduxjs/toolkit';
+import fetchTrades from './budaApi';
 
-let today = new Date()
-today.setUTCHours(12, 0, 0, 0)
-today = today.getTime() 
+let today = new Date();
+today.setUTCHours(12, 0, 0, 0);
+today = today.getTime();
 
-let yearAgo = new Date(today)
-yearAgo.setMonth(yearAgo.getMonth() - 11)
-yearAgo.setUTCHours(12, 0, 0, 0)
-yearAgo = yearAgo.getTime()
+let yearAgo = new Date(today);
+yearAgo.setMonth(yearAgo.getMonth() - 11);
+yearAgo.setUTCHours(12, 0, 0, 0);
+yearAgo = yearAgo.getTime();
 
 const initialState = {
   error: null,
@@ -16,41 +16,56 @@ const initialState = {
   startDate: yearAgo,
   endDate: today,
   dcaData: [],
-  inversion: 100000
-}
+  inversion: 100000,
+  inversion2: 0,
+  market: 'btc-clp',
+};
 
 const tradeSlice = createSlice({
   name: 'trades',
   initialState,
   reducers: {
     setStartDate: (state, action) => {
-      state.startDate = action.payload
+      state.startDate = action.payload;
     },
     setEndDate: (state, action) => {
-      state.endDate = action.payload
+      state.endDate = action.payload;
     },
     setDcaData: (state, action) => {
-      state.dcaData = action.payload
+      state.dcaData = action.payload;
     },
     setInversion: (state, action) => {
-      state.inversion = action.payload
-    }
+      state.inversion = action.payload;
+    },
+    setInversion2: (state, action) => {
+      state.inversion2 = action.payload;
+    },
+    setMarket: (state, action) => {
+      state.market = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchTrades.pending, (state) => {
-        state.isLoading = true
+        state.isLoading = true;
       })
       .addCase(fetchTrades.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.trades = action.payload.trades
+        state.isLoading = false;
+        state.trades = action.payload.trades;
       })
       .addCase(fetchTrades.rejected, (state, action) => {
-        state.isLoading = false
-        state.error = action.error.message
-      })
-  }
-})
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
 
-export const { setStartDate, setEndDate, setDcaData, setInversion } = tradeSlice.actions
-export default tradeSlice.reducer
+export const {
+  setStartDate,
+  setEndDate,
+  setDcaData,
+  setInversion,
+  setInversion2,
+  setMarket,
+} = tradeSlice.actions;
+export default tradeSlice.reducer;
