99b920b4cedcb7d01291b60143ca233fb87aa3dd
Cesar Herrera <cesarherr321@gmail.com>
2024-01-04 18:07:41 -0300

    Update fetch to add coins markets

--- src/components/custom/useFetchTradesForDateRange.jsx
+++ src/components/custom/useFetchTradesForDateRange.jsx
@@ -1,50 +1,61 @@
-import { useDispatch } from 'react-redux'
-import fetchTrades from '/src/redux/trades/budaApi'
-import { setDcaData } from '/src/redux/trades/tradeSlice'
+import { useDispatch, useSelector } from 'react-redux';
+import fetchTrades from '/src/redux/trades/budaApi';
+import { setDcaData } from '/src/redux/trades/tradeSlice';
 
 const useFetchTradesForDateRange = () => {
-  const dispatch = useDispatch()
+  const { market } = useSelector((state) => state.trades);
+  const dispatch = useDispatch();
 
   const fetchTradesForDateRange = async (startDate, endDate, inversion) => {
-    let sumInversion = 0
-    let amount = 0
-    const dates = []
-    const currentDate = new Date(startDate)
-    currentDate.setDate(1)
+    let sumInversion = 0;
+    let amount = 0;
+    const dates = [];
+    const currentDate = new Date(startDate);
+    currentDate.setDate(1);
 
     while (currentDate.getTime() <= endDate) {
+      let fetchData = {
+        date: currentDate.getTime(),
+        market,
+      };
       try {
-        const response = await dispatch(fetchTrades(currentDate.getTime()))
+        const response = await dispatch(fetchTrades(fetchData));
 
         if (response?.payload?.trades?.entries) {
-          const tradesEntries = response.payload.trades.entries
-          sumInversion += Number(inversion)
-          amount += Number(inversion) / Number(tradesEntries[0][2])
+          const tradesEntries = response.payload.trades.entries;
+          sumInversion += Number(inversion);
+          amount += Number(inversion) / Number(tradesEntries[0][2]);
           dates.push({
             date: new Date(currentDate.getTime()).toLocaleString('es-ES', {
               month: 'long',
-              year: 'numeric'
+              year: 'numeric',
             }),
             timestamp: tradesEntries[0][0],
             sumInversion: Number(sumInversion),
             price: Number(tradesEntries[0][2]),
             inversion,
             portfolio: Number(amount) * Number(tradesEntries[0][2]),
-            change: Number(amount) * Number(tradesEntries[0][2]) - Number(sumInversion),
-            percentage: ((Number(amount) * Number(tradesEntries[0][2]) - Number(sumInversion)) / Number(sumInversion)) * 100
-          })
+            change:
+              Number(amount) * Number(tradesEntries[0][2]) -
+              Number(sumInversion),
+            percentage:
+              ((Number(amount) * Number(tradesEntries[0][2]) -
+                Number(sumInversion)) /
+                Number(sumInversion)) *
+              100,
+          });
         }
       } catch (error) {
-        console.error('Error fetching trades for date:', currentDate, error)
+        console.error('Error fetching trades for date:', currentDate, error);
       }
 
-      currentDate.setMonth(currentDate.getMonth() + 1)
+      currentDate.setMonth(currentDate.getMonth() + 1);
     }
 
-    dispatch(setDcaData(dates))
-  }
+    dispatch(setDcaData(dates));
+  };
 
-  return { fetchTradesForDateRange }
-}
+  return { fetchTradesForDateRange };
+};
 
-export default useFetchTradesForDateRange
+export default useFetchTradesForDateRange;
